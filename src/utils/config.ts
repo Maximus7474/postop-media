6be2218377e88e2607ad06511117642630eb1144
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-16 10:38:17 +0100

    tweak(utils/config): added devmode & removed DB related fields

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,21 +4,13 @@ const logger = new Logger('CONFIG');
 const fields = {
     DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN,
     MAIN_GUILD_ID: process.env.MAIN_GUILD_ID,
-
-    // SQLite setup
-    SQLITE_PATH: process.env.SQLITE_PATH,
-
-    // SQL setup
-    SQL_HOST: process.env.SQL_HOST,
-    SQL_PORT: Number(process.env.SQL_PORT) || undefined,
-    SQL_USER: process.env.SQL_USER,
-    SQL_DATABASE: process.env.SQL_DATABASE,
-    SQL_PASSWORD: process.env.SQL_PASSWORD,
+    NODE_ENV: process.env.NODE_ENV ?? 'development',
 };
 
 interface Config {
     DISCORD_BOT_TOKEN: string;
     MAIN_GUILD_ID: string|false;
+    DEV_MODE: boolean;
 };
 
 if (!fields.DISCORD_BOT_TOKEN) {
@@ -32,6 +24,7 @@ if (!fields.MAIN_GUILD_ID) {
 const env: Config = {
     DISCORD_BOT_TOKEN: fields.DISCORD_BOT_TOKEN,
     MAIN_GUILD_ID: fields.MAIN_GUILD_ID ?? false,
+    DEV_MODE: fields.NODE_ENV === 'development',
 }
 
 export default env;
