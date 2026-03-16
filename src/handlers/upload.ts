babc2af065676c116095689ddc785c9de5cabdb9
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-16 12:52:24 +0100

    feat(handlers): initial upload handler

---
+++ src/handlers/upload.ts
@@ -0,0 +1,39 @@
+import config from '../utils/config';
+import Logger from '../utils/logger';
+import type { Uploader } from '../types/uploader';
+import type { GuildMember } from 'discord.js';
+
+const logger = new Logger('UPLOAD');
+
+export class UploadHandler {
+    private uploader: Uploader | null = null;
+    private initializationPromise: Promise<void>;
+
+    constructor() {
+        const { UploadMethod } = config;
+
+        this.initializationPromise = this.loadUploader(UploadMethod);
+    }
+
+    private async loadUploader(method: string) {
+        try {
+            const module = await import(`./providers/${method}`);
+            
+            this.uploader = new module.default();
+            logger.info(`Service [${method}] is ready for media uploads.`);
+        } catch (err) {
+            logger.error(`Failed to load service [${method}]:`, err);
+            throw err;
+        }
+    }
+
+    async upload(blob: Blob, fileName: string, member: GuildMember): Promise<string> {
+        await this.initializationPromise;
+
+        if (!this.uploader) {
+            throw new Error("No uploader initialized. Post-OP Media is offline.");
+        }
+
+        return await this.uploader.uploadBlob(blob, fileName, member);
+    }
+}
